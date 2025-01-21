1f414956e1006a0a204c32be792bc3c80cb261d0
Anagh Neginhal <166278457+NotSomeoneOrMe@users.noreply.github.com>
2025-01-21 14:59:53 -0800

    Toggling

--- OGPC-S18/Assets/Scripts/MapManager.cs
+++ OGPC-S18/Assets/Scripts/MapManager.cs
@@ -1,20 +1,26 @@
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class MapManager : MonoBehaviour
 {
     [SerializeField] private Vector2 worldSize;
-    [SerializeField] private Canvas map;
+    [SerializeField] private GameObject map;
     [SerializeField] private GameObject islandIconReference;
+
+    [SerializeField] private InputActionAsset inputs;
     
+    private InputAction mapToggle;
+    private bool mapOn;
     GameObject[] islands;
     GameObject[] islandIcons;
     float scaleFactor;
     void Start()
     {
+        mapToggle = inputs.FindActionMap("Player").FindAction("MapToggle");
+        mapToggle.Enable();
+
         islands = GameObject.FindGameObjectsWithTag("Island");
         islandIcons = new GameObject[islands.Length];
         scaleFactor = DetermineMapScaleFactor();
-
 
         Vector2 islandCoords;
         for (int i = 0; i < islands.Length; i++)
@@ -33,8 +39,9 @@ public class MapManager : MonoBehaviour
         float xFactor;
         float yFactor;
         float realFactor;
+        RectTransform mapRect = map.GetComponent<RectTransform>();
 
-        Vector2 mapSize = new Vector2(2*454,2*230);
+        Vector2 mapSize = new Vector2(mapRect.rect.width,mapRect.rect.height);
         xFactor = worldSize.x/mapSize.x;
         yFactor = worldSize.y/mapSize.y;
         realFactor = 1 / Mathf.Max(xFactor,yFactor);
@@ -45,6 +52,19 @@ public class MapManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (mapToggle.triggered)
+        {
+            if (mapOn)
+            {
+                mapOn = false;
+                mapToggle.Disable();
+            }
+            else
+            {
+                mapOn = true;
+                mapToggle.Enable();
+            }
+            map.SetActive(mapOn);
+        }
     }
 }
