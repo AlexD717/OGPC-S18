2326baf7d77983e10a8d65d04bb1f97e65829eb9
AlexD717 <alex717nd@gmail.com>
2025-03-24 16:27:07 -0700

    Simple Lose Screen

--- OGPC-S18/Assets/Scripts/LevelManager.cs
+++ OGPC-S18/Assets/Scripts/LevelManager.cs
@@ -79,25 +79,48 @@ public class LevelManager : MonoBehaviour
     private void ShowEndScreen()
     {
         showEndScreen = true;
+        // Hide all other items in UI
         foreach (Transform child in winScreen.GetComponentInParent<Transform>())
         {
             child.gameObject.SetActive(false);
         }
+        
         if (playerLost)
         {
-            loseScreen.SetActive(true);
-            foreach (Transform child in loseScreen.transform)
-            {
-                child.gameObject.SetActive(true);
-            }
+            ShowLoseScreen();
         }
         else
         {
-            winScreen.SetActive(true);
-            foreach (Transform child in winScreen.transform)
-            {
-                child.gameObject.SetActive(true);
-            }
+            ShowWinScreen();
+        }
+    }
+
+    private void ShowWinScreen()
+    {
+        winScreen.SetActive(true);
+        foreach (Transform child in winScreen.transform)
+        {
+            child.gameObject.SetActive(true);
+        }
+
+        // Fill in extra information
+        Transform dataGrid = winScreen.transform.GetChild(2);
+        // Fill in score depending on how much time is left
+        dataGrid.GetChild(4).GetComponent<TextMeshProUGUI>().text = countdownText.text; // Says time remaining
+        float timeRemainingScore = Mathf.Round(Mathf.Log(countdown, 1.7f) * 250);
+        dataGrid.GetChild(5).GetComponent<TextMeshProUGUI>().text = timeRemainingScore.ToString();
+
+        // Fill in total score
+        float totalScore = timeRemainingScore;
+        winScreen.transform.GetChild(3).GetComponent<TextMeshProUGUI>().text = "Total Score: " + totalScore.ToString();
+    }
+
+    private void ShowLoseScreen()
+    {
+        loseScreen.SetActive(true);
+        foreach (Transform child in loseScreen.transform)
+        {
+            child.gameObject.SetActive(true);
         }
     }
 
