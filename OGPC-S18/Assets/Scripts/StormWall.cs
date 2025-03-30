e29eda58cb31b9c69d07159ded85c6da3a93290f
Anagh Neginhal <166278457+NotSomeoneOrMe@users.noreply.github.com>
2025-03-30 10:41:07 -0700

    Stormwall

---
+++ OGPC-S18/Assets/Scripts/StormWall.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class StormWall : MonoBehaviour
+{
+    [Header("stormWall Condigurables")]
+    [SerializeField] Vector2 startPosition;
+    [SerializeField] float startRotation;
+    [SerializeField] Vector2 startVelocity;
+    [SerializeField] Vector2 startAcceleration;
+    [SerializeField] bool stormWallFacesDirectionOfTravel = true;
+    private Rigidbody2D stormWallRigidBody;
+
+    private GameObject stormWall;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        stormWall = transform.gameObject;
+        stormWallRigidBody = stormWall.GetComponent<Rigidbody2D>();
+        stormWall.transform.position = startPosition;
+        stormWall.transform.rotation = Quaternion.Euler(0, 0, startRotation);
+        stormWallRigidBody.linearVelocity = startVelocity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        stormWallRigidBody.linearVelocity += startAcceleration * Time.deltaTime;
+        if (stormWallFacesDirectionOfTravel)
+        {
+            stormWall.transform.rotation = Quaternion.Euler(0, 0, UsefulStuff.Convert.VectorToPolar(stormWallRigidBody.linearVelocity)[0]);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            LevelManager levelManager = FindFirstObjectByType<LevelManager>();
+            levelManager.PlayerLost();
+        }
+    }
+}
